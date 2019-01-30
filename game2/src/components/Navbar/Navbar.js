d7132c3781232a7669fda157d45ea67c01404631
zevans98 <zman6221998@gmail.com>
2019-01-29 17:52:17 -0700

    animals

---
+++ game2/src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Navbar.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top">
+         <ul>
+          <li className="itemLeft">Test Your Memory</li>
+          <li className="itemCenter"></li>
+          <li className="itemRight">Score: {this.props.score}</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
---
+++ game2/src/components/friendcard/friendcard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./friendcard.css";
+
+//pass the image into each card so all 12 are rendered
+const FriendCard = props => (
+  <div className="card" onClick={props.imageClick}>
+    <div className="img-container">
+      <img alt={props.image.replace(".jpg", "")} src={require("../../images/" + props.image)} />
+    </div>
+  </div>
+);
+
+export default FriendCard;
